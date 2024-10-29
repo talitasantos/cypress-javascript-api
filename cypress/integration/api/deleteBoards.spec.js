47ab5b5c60ca6992a00a458e1144eb3e1804193a
talitasantos2310@gmail.com <Maycon@1802>
2024-10-29 19:57:48 -0300

    deleteBoard

--- cypress/integration/api/deleteBoards.spec.js
+++ cypress/integration/api/deleteBoards.spec.js
@@ -1,47 +1,70 @@
-// /// <reference types="Cypress" />
+/// <reference types="Cypress" />
 
-// describe('Trello Boards API Tests', () => {
-//   const baseUrl = `${Cypress.config('baseUrl')}/boards/`;
-//   const defaultQueryParams = {
-//     token: Cypress.config('token'),
-//     key: Cypress.config('apiKey')
-//   };
+describe('Trello Delete Boards API Tests', () => {
+    const baseUrl = `${Cypress.config('baseUrl')}/boards/`;
+    const defaultQueryParams = {
+        token: Cypress.config('token'),
+        key: Cypress.config('apiKey')
+    };
 
-//   const createBoardRequest = (queryParams) => {
-//     return cy.request({
-//       method: 'POST',
-//       url: baseUrl,
-//       qs: { ...defaultQueryParams, ...queryParams },
-//       failOnStatusCode: false
-//     });
-//   };
+    const createBoardRequest = (queryParams) => {
+        return cy.request({
+            method: 'POST',
+            url: baseUrl,
+            qs: { ...defaultQueryParams, ...queryParams },
+            failOnStatusCode: false
+        });
+    };
 
-//   it('Successfully create a new board', () => {
-//     createBoardRequest({ name: 'Board created by Robot' }).then((response) => {
-//       expect(response.status).to.eq(200);
+    const deleteBoardRequest = (boardId) => {
+        return cy.request({
+            method: 'DELETE',
+            url: `${baseUrl}${boardId}`,
+            qs: defaultQueryParams,
+            failOnStatusCode: false
+        });
+    };
 
-//       // Stores the ID of the created board
-//       cy.wrap(response.body.id).as('boardId');
+    it('Successfully create a new board and delete it', () => {
+        createBoardRequest({ name: 'Board created by Robot to be deleted' }).then((response) => {
+            expect(response.status).to.eq(200);
 
-//       cy.request('DELETE', `${boardId}`).then((response) => {
-//         expect(response.status).to.eq(200);
-//         expect(response.body).to.have.property('_value', null);
-//       });
+            // Stores the ID of the board created to delete later
+            const boardId = response.body.id;
+            cy.wrap(boardId).as('id');
 
-//     });
-//   });
+            // Delete the created board
+            deleteBoardRequest(boardId).then((deleteResponse) => {
+                expect(deleteResponse.status).to.eq(200);
+            });
+        });
+    });
 
-//   // it('should delete the board', () => {
-//   //   cy.request('DELETE', `${boardId}`).then((response) => {
-//   //     expect(response.status).to.eq(200);
-//   //     expect(response.body).to.have.property('_value', null);
-//   //   });
-//   // });
+    it('Attempt to delete a invalid board', () => {
+        deleteBoardRequest({ boardId: '67201e3569ca1487b2ff4f40FXXX' }).then((response) => {
+            expect(response.status).to.eq(400);
+            expect(response.body).to.eq('invalid id');
+        });
+    });
 
-//   // it('should verify the user is deleted', () => {
-//   //   cy.request({ method: 'GET', url: `${boardId}`, failOnStatusCode: false })
-//   //     .then((response) => {
-//   //       expect(response.status).to.eq(404);
-//   //     });
-//   // });
-// });
+    // it('Attempt to delete a non-existing board', () => {
+    //     deleteBoardRequest({ boardId: '67201a6a0d25adc27aae7a6c' }).then((response) => {
+    //         expect(response.status).to.eq(404);
+    //         expect(response.body).to.eq('The requested resource was not found.');
+    //     });
+    // });
+
+    // it('Attempt to delete a board with an invalid Token', () => {
+    //     deleteBoardRequest({ boardId: '67201a6a0d25adc27aae7a6c', token: 'invalidToken' }).then((deleteResponse) => {
+    //         expect(deleteResponse.status).to.eq(401);
+    //         eexpect(response.body).to.eq('unauthorized permission requested');
+    //     });
+    // });
+
+    // it('Attempt to delete a board with an invalid API Key', () => {
+    //     deleteBoardRequest({ boardId: '67201a6a0d25adc27aae7a6c', key: 'invalidKey' }).then((deleteResponse) => {
+    //         expect(deleteResponse.status).to.eq(401);
+    //         expect(deleteResponse.body).to.eq('invalid app key');
+    //     });
+    // });
+});
