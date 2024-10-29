6bdd1df2f213d5f233c90328c127e40bdf48001d
talitasantos2310@gmail.com <Maycon@1802>
2024-10-28 21:23:05 -0300

    create cards tests

--- cypress/integration/api/createCards.spec.js
+++ cypress/integration/api/createCards.spec.js
@@ -1,24 +1,57 @@
 /// <reference types="Cypress" />
 
 describe('Trello Cards API Tests', () => {
-  const baseUrl = `${Cypress.config('baseUrl')}/cards/`;
-  const defaultQueryParams = {
-    token: Cypress.config('token'),
-    key: Cypress.config('apiKey')
-  };
+    const baseUrl = `${Cypress.config('baseUrl')}/cards/`;
+    const defaultQueryParams = {
+        token: Cypress.config('token'),
+        key: Cypress.config('apiKey')
+    };
 
-  const createCardRequest = (queryParams) => {
-    return cy.request({
-      method: 'POST',
-      url: baseUrl,
-      qs: { ...defaultQueryParams, ...queryParams },
-      failOnStatusCode: false
-    });
-  };
+    const createCardRequest = (queryParams) => {
+        return cy.request({
+            method: 'POST',
+            url: baseUrl,
+            qs: { ...defaultQueryParams, ...queryParams },
+            failOnStatusCode: false
+        });
+    };
 
-  it('Successfully create a new card', () => {
-    createCardRequest({ name: 'Card created by Robot' , idList: '671d789c89e6c35196d5d940'}).then((response) => {
-      expect(response.status).to.eq(200);
+    it('Successfully create a new card', () => {
+        createCardRequest({ name: 'Card created by Robot', idList: '671d789c89e6c35196d5d940' }).then((response) => {
+            expect(response.status).to.eq(200);
+        });
     });
-  });
+
+    it('Attempt to create a card without a idList', () => {
+        createCardRequest({}).then((response) => {
+            expect(response.status).to.eq(400);
+            expect(response.body).to.eq('invalid value for idList');
+        });
+    });
+
+    it('Attempt to create a card with non-existent idList', () => {
+        createCardRequest({ idList: '671d789c89e6c35196d5d948' }).then((response) => {
+            expect(response.status).to.eq(404);
+            expect(response.body).to.eq('could not find the board that the card belongs to');
+        });
+    });
+
+    it('Attempt to create a card with an invalid Token', () => {
+        createCardRequest({
+            token: 'invalidToken',
+            idList: '671d789c89e6c35196d5d940'
+        }).then((response) => {
+            expect(response.status).to.eq(401);
+        });
+    });
+
+    it('Attempt to create a card with an invalid API Key', () => {
+        createCardRequest({
+            key: '7085f6851a2fab9dc40545959882364d',
+            idList: '671d789c89e6c35196d5d940'
+        }).then((response) => {
+            expect(response.status).to.eq(401);
+            expect(response.body).to.eq('invalid key');
+        });
+    });
 });
